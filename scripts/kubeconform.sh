dcdadf39e51b5a67c0b3939eb8e45fe92c5b4cb1
chaijunkin <chaijunkin@gmail.com>
2024-04-07 11:13:01 +0800

    remove

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -33,16 +33,16 @@ kubeconform_args=(
 #     fi
 # done
 
-echo "=== Validating kustomizations in ${KUBERNETES_DIR}/flux ==="
-find "${KUBERNETES_DIR}/flux" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
-  do
-    echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
-    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
-      kubeconform "${kubeconform_args[@]}"
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-      exit 1
-    fi
-done
+# echo "=== Validating kustomizations in ${KUBERNETES_DIR}/flux ==="
+# find "${KUBERNETES_DIR}/flux" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
+#   do
+#     echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
+#     kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
+#       kubeconform "${kubeconform_args[@]}"
+#     if [[ ${PIPESTATUS[0]} != 0 ]]; then
+#       exit 1
+#     fi
+# done
 
 echo "=== Validating kustomizations in ${KUBERNETES_DIR}/apps ==="
 find "${KUBERNETES_DIR}/apps" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
