586bde3935b99f2d8a15854f3884b04795b75c65
chaijunkin <chaijunkin@gmail.com>
2025-08-17 11:28:32 +0800

    fix: expose minio via gateway

--- kubernetes/apps/kube-system/force-reconcile-helmrelease-parallel.fish
+++
@@ -1,6 +0,0 @@
-for line in (kubectl get helmrelease -A -o custom-columns="NAMESPACE:.metadata.namespace,NAME:.metadata.name" --no-headers)
-    set ns (echo $line | awk '{print $1}')
-    set name (echo $line | awk '{print $2}')
-    flux reconcile hr -n $ns $name --force &
-end
-wait
