a0f5bf60ff2bfd63dbf9d469aeb0a0dc7a678f5d
chaijunkin <chaijunkin@gmail.com>
2025-09-02 14:58:54 +0800

    fix: add draft for cloudflare 5.x

--- infrastructure/terraform/cloudflare/main.tf
+++ infrastructure/terraform/cloudflare/main.tf
@@ -1,51 +1,55 @@
 resource "cloudflare_zone" "cloudflare_zone" {
-  account_id = local.cloudflare_account_id
-  zone       = local.cloudflare_zone_name
+  account = {
+    id = local.cloudflare_account_id
+  }
+  name       = local.cloudflare_zone_name
 }
 
 resource "cloudflare_zone_dnssec" "cloudflare_zone_dnssec" {
   zone_id = cloudflare_zone.cloudflare_zone.id
 }
 
-resource "cloudflare_record" "records" {
+resource "cloudflare_dns_record" "records" {
   for_each = local.cloudflare_record
   zone_id  = cloudflare_zone.cloudflare_zone.id
   name     = each.value.name
-  value    = each.value.value
+  content  = each.value.value
   type     = each.value.type
   priority = try(each.value.priority, null)
   proxied  = try(each.value.proxied, false)
-  ttl      = try(each.value.ttl, null)
+  ttl      = try(each.value.ttl, 1)
 }
 
-resource "cloudflare_record" "github_A_record" {
+resource "cloudflare_dns_record" "github_A_record" {
   for_each = toset(local.github_A_record)
   zone_id  = cloudflare_zone.cloudflare_zone.id
   name     = "@"
-  value    = each.value
+  content  = each.value
+  ttl      = try(each.value.ttl, 1)
   type     = "A"
   proxied  = true
 }
 
-resource "cloudflare_record" "github_AAAA_record" {
+resource "cloudflare_dns_record" "github_AAAA_record" {
   for_each = toset(local.github_AAAA_record)
   zone_id  = cloudflare_zone.cloudflare_zone.id
   name     = "@"
-  value    = each.value
+  content  = each.value
+  ttl      = try(each.value.ttl, 1)
   type     = "AAAA"
   proxied  = true
 }
 
 
-resource "random_id" "tunnel_secret" {
-  byte_length = 35
-}
+# resource "random_id" "tunnel_secret" {
+#   byte_length = 35
+# }
 
-resource "cloudflare_tunnel" "default" {
-  account_id = local.cloudflare_account_id
-  name       = "profile-cloudflare-tunnel"
-  secret     = random_id.tunnel_secret.b64_std
-}
+# resource "cloudflare_tunnel" "default" {
+#   account_id = local.cloudflare_account_id
+#   name       = "profile-cloudflare-tunnel"
+#   secret     = random_id.tunnel_secret.b64_std
+# }
 
 # # Creates the CNAME record that routes http_app.${var.cloudflare_zone} to the tunnel.
 # resource "cloudflare_record" "me" {
--- infrastructure/terraform/cloudflare/outputs.tf
+++ infrastructure/terraform/cloudflare/outputs.tf
@@ -1,7 +1,7 @@
-output "cloudflare_tunnel_id" {
-  value = cloudflare_tunnel.default.id
-}
+# output "cloudflare_tunnel_id" {
+#   value = cloudflare_tunnel.default.id
+# }
 
-output "cloudflare_tunnel_cname" {
-  value = cloudflare_tunnel.default.cname
-}
+# output "cloudflare_tunnel_cname" {
+#   value = cloudflare_tunnel.default.cname
+# }
