12345450cb2e2f191cad2174c3a5604c295cd943
chaijunkin <chaijunkin@gmail.com>
2026-02-17 23:13:03 +0800

    fix: migrate state for terraform cloud to cloudflare r2

--- infrastructure/terraform/s3/backblaze/providers.tf
+++ infrastructure/terraform/s3/backblaze/providers.tf
@@ -1,7 +1,19 @@
 terraform {
-  cloud {
-    organization = "chaijunkin"
-    workspaces { name = "b2-workspace" }
+  backend "s3" {
+    bucket       = "tf-state-bucket-cloudjur-com"
+    key          = "backblaze.terraform.tfstate"
+    use_lockfile = true
+    endpoints = {
+      s3 = "https://8550295d25a8172e9fe4f9f7a7f327be.r2.cloudflarestorage.com"
+    }
+
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
   }
   required_providers {
     b2 = {
