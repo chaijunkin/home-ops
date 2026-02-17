dee9e8e202cde4336deafadc058d5792ecfdd534
chaijunkin <chaijunkin@gmail.com>
2026-02-17 22:45:29 +0800

    fix: add missing

--- infrastructure/terraform/authentik/main.tf
+++ infrastructure/terraform/authentik/main.tf
@@ -4,7 +4,23 @@ terraform {
       source  = "goauthentik/authentik"
       version = "2025.12.1"
     }
+  }
 
+  backend "s3" {
+    bucket   = "tf-state-bucket-cloudjur-com"
+    key      = "authentik.terraform.tfstate"
+    use_lockfile = true
+    endpoints = {
+      s3 = "https://8550295d25a8172e9fe4f9f7a7f327be.r2.cloudflarestorage.com"
+    }
+
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
   }
 }
 
@@ -12,33 +28,3 @@ provider "authentik" {
   url   = "https://auth.${var.public_domain}"
   token = var.authentik_token
 }
-
-terraform {
-  cloud {
-    organization = "chaijunkin"
-    workspaces { name = "authentik-workspace" }
-  }
-}
-
-# terraform {
-#   backend "s3" {
-#     bucket   = "tf-state-bucket-cloudjur-com"
-#     key      = "authentik.terraform.tfstate"
-#     use_lockfile = true
-#     endpoints = {
-#       s3 = "https://8550295d25a8172e9fe4f9f7a7f327be.r2.cloudflarestorage.com"
-#     }
-
-#     region                      = "auto"
-#     skip_credentials_validation = true
-#     skip_metadata_api_check     = true
-#     skip_region_validation      = true
-#     skip_requesting_account_id  = true
-#     skip_s3_checksum            = true
-#     use_path_style              = true
-#   }
-#   # cloud {
-#   #   organization = "chaijunkin"
-#   #   workspaces { name = "authentik-workspace" }
-#   # }
-# }
