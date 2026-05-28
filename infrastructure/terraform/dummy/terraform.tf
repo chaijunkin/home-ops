418a85a9b555825db95d5d67caa3bd84adc7c2df
chaijunkin <chaijunkin@gmail.com>
2026-05-28 15:40:45 +0800

    fix: test tofu-controller

---
+++ infrastructure/terraform/dummy/terraform.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "s3" {
+    bucket       = "tf-state-bucket-cloudjur-com"
+    key          = "dummy.terraform.tfstate"
+    use_lockfile = true
+    endpoints = {
+      s3 = "https://8550295d25a8172e9fe4f9f7a7f327be.r2.cloudflarestorage.com"
+    }
+
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+  }
+}
+
+resource "null_resource" "no_code" {
+
+}
+
+output "dummy_output" {
+  value = "This is a dummy output"
+}
