14e6c522ac7c27c0bc8a7b2f6aabf78ff9f6cabf
chaijunkin <88609082+chaijunkin@users.noreply.github.com>
2026-08-10 00:21:44 +0800

    Update directory.tf

--- infrastructure/terraform/authentik/directory.tf
+++ infrastructure/terraform/authentik/directory.tf
@@ -84,6 +84,7 @@ resource "authentik_source_oauth" "google" {
   slug                = "google"
   authentication_flow = data.authentik_flow.default-source-authentication.id
   enrollment_flow     = authentik_flow.enrollment-google.uuid
+# authentik_flow.enrollment-google.uuid
   user_matching_mode  = "email_link"
 
   provider_type   = "google"
